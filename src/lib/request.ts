7688114ce12a5f13feee5e842625717a60b76657
s_andrey <weo9345@gmail.com>
2022-07-29 09:50:01 +0300

    Add utilities to make api requests

---
+++ src/lib/request.ts
@@ -0,0 +1,88 @@
+type QueryParams = {
+  [key: string]: string | number | string[] | undefined;
+};
+
+type RetryOptions = {
+  retryCount: number;
+  retryDelay: number;
+};
+
+const defaultRetryOptions: RetryOptions = {
+  retryCount: 2,
+  retryDelay: 2000,
+};
+
+const queryParams = ( params?: QueryParams ) => {
+  if ( !params ) {
+      return '';
+  }
+
+  const result = new URLSearchParams();
+
+  Object.entries( params ).forEach( ( [key, value] ) => {
+      if ( typeof value === 'undefined' ) {
+          return;
+      }
+
+      if ( Array.isArray( value ) ) {
+          return value.forEach( valueItem => result.append( key, valueItem ) );
+      }
+
+      result.append( key, value.toString() );
+  });
+
+  return result.toString();
+}
+
+const fetchRetry = async ( url: string, init?: RequestInit, retryOptions: RetryOptions = defaultRetryOptions ) => {
+  const { retryCount, retryDelay } = retryOptions;
+
+  for ( let attempt = 0; attempt <= Math.max( retryCount, 0 ); attempt++ ) {
+      try {
+          return await fetch(url, init);
+      } catch ( err ) {
+          if ( attempt === retryCount ) {
+              throw err;
+          }
+
+          if ( retryDelay ) {
+              await new Promise(resolve => setTimeout(resolve, retryDelay));
+          }
+      }
+  }
+
+  return Promise.reject( 'Unable to fetch. Check retryOptions values' );
+}
+
+/**
+ * request for some resourse
+ * @param {string} url 
+ * @param {RequestInit} [options] 
+ * @returns {Promise<object>} response of fetch in json
+ */
+const request = async <T extends object>( url: string, options?: RequestInit ): Promise<T> => {
+  const response = await fetchRetry( url, options );
+
+  if ( response.ok ) {
+    return response.json();
+  }
+
+  return Promise.reject( response );
+}
+
+/**
+ * get request
+ * @param {string} host - e.g http://example.com/
+ * @param {string} path - e.g /some/path
+ * @param {QueryParams} [query] - object of query params (key: value)
+ * @param {RequestInit} [options]
+ * @returns {Promise<object>} response of get request
+ */
+export const get = <T extends object>(
+  host: string,
+  path: string,
+  query?: QueryParams,
+  options?: RequestInit
+): Promise<T> => {
+  return request( `${host}${path}${ queryParams( query ) ? `?${ queryParams( query ) }` : ''}`, options );
+}
