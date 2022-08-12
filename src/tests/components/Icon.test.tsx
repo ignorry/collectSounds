932ae6a29f55428ad39d8696b58ad91a64598a7d
s_andrey <weo9345@gmail.com>
2022-08-12 19:31:38 +0300

    Add Icon componenet
    display icon from sprite svg

---
+++ src/tests/components/Icon.test.tsx
@@ -0,0 +1,35 @@
+import React from "react";
+import { render, fireEvent, waitFor, screen } from "@testing-library/react";
+import { ThemeProvider } from "styled-components";
+import { getTheme } from "../../lib/theme";
+import GlobalStyles from "../../lib/styled/global";
+
+import Icon from "../../components/primitives/Icon";
+
+describe( 'Icon componenet', () => {
+  it( 'renders icon', () => {
+    const result = render(
+      <ThemeProvider theme={ getTheme( true ) } >
+        <Icon name="icon"/>
+        <GlobalStyles/>
+      </ThemeProvider>
+    );
+
+    const elem = result.container.querySelector('use');
+
+    expect( elem.getAttribute( 'href' ) ).toBe( 'icons.svg#icon' );
+  });
+
+  it( 'renders icon with active param', () => {
+    const result = render(
+      <ThemeProvider theme={ getTheme( true ) } >
+        <Icon name="icon" active={true}/>
+        <GlobalStyles/>
+      </ThemeProvider>
+    );
+
+    const elem = result.container.querySelector('use');
+
+    expect( elem.getAttribute( 'href' ) ).toBe( 'icons.svg#icon' );
+  });
+});
