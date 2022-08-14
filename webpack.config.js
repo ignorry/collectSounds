ad6ffabbedae62a53a983f5e15efd98920c4a1c6
s_andrey <weo9345@gmail.com>
2022-08-14 21:15:07 +0300

    Update webpack config
    Add copy webpack plugin for copy /public to /dist on production build

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,6 @@
 const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
+const CopyWebpackPlugin = require("copy-webpack-plugin");
 
 module.exports = {
   entry: "./src/index.tsx",
@@ -23,6 +24,16 @@ module.exports = {
   plugins: [
     new HtmlWebpackPlugin({
       template: "./public/index.html"
+    }),
+    new CopyWebpackPlugin({
+      patterns: [
+        { 
+          from: "public",
+          globOptions: {
+            ignore: ["**/*.html"],
+          },
+        }
+      ],
     })
   ],
   resolve: {
