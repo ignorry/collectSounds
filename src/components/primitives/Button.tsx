02fe86bc0881a6f038bbc1a35cfa1aabc8c68dc3
s_andrey <weo9345@gmail.com>
2022-08-14 22:17:00 +0300

    Update Button component
    remove hover animation

--- src/components/primitives/Button.tsx
+++ src/components/primitives/Button.tsx
@@ -10,20 +10,18 @@ export const ButtonStyled = styled.button`
   align-items: center;
   border: none;
   color: ${ ({ theme }) => theme.colors.font };
-
-  &:hover {
-    opacity: 0.5;
-  }
 `;
 
 /**
  * @typedef Props
  * @prop {Function} callback - callback on click
+ * @prop {boolean} secondary - use seconday label
  * @prop {string} pic - name of icon in sprite
  * @prop {string} text - text to be displayed in button
  */
 export type Props = {
   callback: Function,
+  secondary?: boolean,
   pic?: string,
   text?: string,
 }
@@ -31,7 +29,7 @@ export type Props = {
 const Button: React.FC<Props> = ( props: Props ) =>
   <ButtonStyled onClick={ () => props.callback() }>
     { props.pic && <Icon name={ props.pic }/> }
-    { props.text && <Label text={ props.text }/> }
+    { props.text && <Label text={ props.text } secondary={ props.secondary }/> }
   </ButtonStyled>
 
 export default Button;
