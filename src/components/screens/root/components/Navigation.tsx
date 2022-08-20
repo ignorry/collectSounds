dc6cc372da888237fd54d34957af52fdb567494a
s_andrey <weo9345@gmail.com>
2022-08-20 23:54:48 +0300

    Add Navigation component

---
+++ src/components/screens/root/components/Navigation.tsx
@@ -0,0 +1,56 @@
+import React, { ReactNode, useEffect, useState } from "react";
+import styled from "styled-components";
+import { useNavigate } from "react-router-dom";
+
+import ToggleButton from "../../../primitives/ToggleButton";
+
+export const Nav = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: flex-start;
+  font-weight: bold;
+  gap: ${ ({ theme }) => `${ theme.gaps.big }rem` };
+  padding: ${ ({ theme }) => `${ theme.gaps.big }rem` };
+  background: ${ ({ theme }) => theme.colors.bgSecondary };
+
+  @media ${ ({ theme }) => theme.media.small } {
+    flex-direction: row;
+    justify-content: space-around;
+  }
+`;
+
+const Navigation: React.FC = () => {
+  const [route, setRoute] = useState<string>( 'search' );
+  const navigate = useNavigate();
+
+  useEffect( () => {
+    setRoute( window.location.pathname.split( '/' )[1] );
+  }, [window.location.pathname]);
+
+  return (
+    <Nav>
+      <ToggleButton
+        callback={ () => navigate( '/search' ) }
+        active={ route === '' || route === 'search' }
+        pic="search"
+      />
+      <ToggleButton
+        callback={ () => navigate( '/saved' ) }
+        active={ route === 'saved' }
+        pic="favorite"
+      />
+      <ToggleButton
+        callback={ () => navigate( '/queue' ) }
+        active={ route === 'queue' }
+        pic="queue"
+      />
+      <ToggleButton
+        callback={ () => navigate( '/settings' ) }
+        active={ route === 'settings' }
+        pic="settings"
+      />
+    </Nav>
+  )
+};
+
+export default Navigation;
---
+++ src/tests/components/Navigation.test.tsx
@@ -0,0 +1,71 @@
+import React from "react";
+import { render, screen } from "@testing-library/react";
+import { ThemeProvider } from "styled-components";
+import { getTheme } from "../../lib/theme";
+import GlobalStyles from "../../lib/styled/global";
+import { Provider } from "react-redux";
+import store from "../../redux/index";
+import { LangProvider } from "../../lib/localization";
+import { BrowserRouter } from "react-router-dom";
+
+import Navigation from "../../components/screens/root/components/Navigation";
+
+const AllTheProviders = ( { children }: any ) => (
+  <Provider store={store}>
+    <ThemeProvider theme={ getTheme( true ) } >
+      <BrowserRouter>
+        <LangProvider file="root">
+          { children }
+        </LangProvider>
+      </BrowserRouter>
+      <GlobalStyles/>
+    </ThemeProvider>
+  </Provider>
+);
+
+describe( 'Error component', () => {
+  it( 'render all buttons', async () => {
+    const result = render(
+      <Navigation />,
+      { wrapper: AllTheProviders }
+    );
+
+    await new Promise( resolve => setTimeout( resolve, 10 ) );
+
+    const icons = result.container.querySelectorAll( 'button' );
+
+    expect( icons ).toHaveLength( 4 );
+  });
+
+  it( 'handles clicks', async () => {
+    const result = render(
+      <Navigation />,
+      { wrapper: AllTheProviders }
+    );
+
+    await new Promise( resolve => setTimeout( resolve, 10 ) );
+
+    result.container.querySelectorAll( 'button' )[0].click();
+    expect( window.location.pathname.split( '/' )[1] ).toBe( 'search' );
+
+    result.container.querySelectorAll( 'button' )[1].click();
+    expect( window.location.pathname.split( '/' )[1] ).toBe( 'saved' );
+    
+    result.container.querySelectorAll( 'button' )[2].click();
+    expect( window.location.pathname.split( '/' )[1] ).toBe( 'queue' );
+
+    result.container.querySelectorAll( 'button' )[3].click();
+    expect( window.location.pathname.split( '/' )[1] ).toBe( 'settings' );
+  });
+
+  it( 'matches to snapshot', async () => {
+    const result = render(
+      <Navigation />,
+      { wrapper: AllTheProviders }
+    );
+
+    await new Promise( resolve => setTimeout( resolve, 10 ) );
+
+    expect( result ).toMatchSnapshot();
+  });
+});
