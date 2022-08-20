b5d287306eeb1d2b0e01bc8d5c390f70aec6b4e9
s_andrey <weo9345@gmail.com>
2022-08-20 20:35:59 +0300

    Modify Button component
    Add active prop

--- src/components/primitives/Button.tsx
+++ src/components/primitives/Button.tsx
@@ -15,20 +15,22 @@ export const ButtonStyled = styled.button`
 /**
  * @typedef Props
  * @prop {Function} callback - callback on click
- * @prop {boolean} secondary - use seconday label
- * @prop {string} pic - name of icon in sprite
- * @prop {string|ReactNode} text - text to be displayed in button
+ * @prop {boolean} [secondary] - use seconday label
+ * @prop {boolean} [active] - use active icon
+ * @prop {string} [pic] - name of icon in sprite
+ * @prop {string} [text] - text to be displayed in button
  */
 export type Props = {
   callback: Function,
   secondary?: boolean,
+  active?: boolean;
   pic?: string,
-  text?: string | ReactNode,
+  text?: string,
 }
 
 const Button: React.FC<Props> = ( props: Props ) =>
   <ButtonStyled onClick={ () => props.callback() }>
-    { props.pic && <Icon name={ props.pic }/> }
+    { props.pic && <Icon name={ props.pic } active={ props.active }/> }
     { props.text && <Label text={ props.text } secondary={ props.secondary }/> }
   </ButtonStyled>
 
