2c945a62f0ad429cffe958527dcaa4594e84ed7a
s_andrey <weo9345@gmail.com>
2022-08-21 16:33:42 +0300

    Add SearchQueries component

---
+++ src/components/screens/search/components/SearchQueries.tsx
@@ -0,0 +1,96 @@
+import React, { useState } from "react";
+import styled from "styled-components";
+import { useIntl } from "react-intl";
+import { SearchFilters } from "../../../../lib/api";
+
+import Input from "../../../primitives/Input";
+import Collapse from "../../../primitives/Collapse";
+import Dropdown from "../../../primitives/Dropdown";
+import Button from "../../../primitives/Button";
+
+const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: ${ ({ theme }) => `${ theme.gaps.big }rem` };
+  padding: ${ ({ theme }) => `${ theme.gaps.big }rem` };
+  margin: auto;
+  background: ${ ({ theme }) => theme.colors.bgSecondary };
+  max-width: ${ ({ theme }) => `${ theme.contentWidth }rem`};
+`;
+
+const InputWrapper = styled.div`
+  display: flex;
+  gap: ${ ({ theme }) => `${ theme.gaps.big }rem` };
+
+  input {
+    width: 100%;
+  }
+`;
+
+export type Props = {
+  callback: Function;
+}
+
+const SearchQueries: React.FC<Props> = ( props: Props ) => {
+  const intl = useIntl();
+  const [q, setQ] = useState<SearchFilters["q"] | null>( null );
+  const [order, setOrder] = useState<SearchFilters["order"]>( 'relevance' );
+  const [type, setType] = useState<SearchFilters["type"] | 'all'>( 'all' );
+  const [duration, setDuration] = useState<SearchFilters["videoDuration"] | 'any'>( 'any' );
+
+  const handleSearch = () => {
+    const filters: SearchFilters = {
+      order: order,
+    };
+    if ( q ) filters.q = q;
+    if ( type && type !== 'all' ) filters.type = type;
+    if ( duration && duration !== 'any' ) filters.videoDuration = duration;
+
+    props.callback( filters );
+  };
+
+  return (
+    <Container>
+      <InputWrapper>
+        <Input value={ q } callback={ setQ } placeholder={ intl.formatMessage({ id: 'input' }) }/>
+        <Button callback={ handleSearch } pic="search" active/>
+      </InputWrapper>
+      <Collapse label={ intl.formatMessage({ id: 'filters' })} icon="filters" dontHandleOutside>
+        <Dropdown 
+          callback={ setOrder }
+          activeVal={ intl.formatMessage({ id: order }) } 
+          values={[
+            intl.formatMessage({ id: 'date' }),
+            intl.formatMessage({ id: 'rating' }),
+            intl.formatMessage({ id: 'relevance' }),
+            intl.formatMessage({ id: 'title' }),
+            intl.formatMessage({ id: 'videoCount' }),
+            intl.formatMessage({ id: 'viewCount' }),
+          ]}
+        />
+        <Dropdown 
+          callback={ setType } 
+          activeVal={ intl.formatMessage({ id: type }) } 
+          values={[
+            intl.formatMessage({ id: 'all' }),
+            intl.formatMessage({ id: 'video' }),
+            intl.formatMessage({ id: 'playlist' }),
+            intl.formatMessage({ id: 'channel' }),
+          ]}
+        />
+        <Dropdown 
+          callback={ setDuration}
+          activeVal={ intl.formatMessage({ id: duration }) } 
+          values={[
+            intl.formatMessage({ id: 'any' }),
+            intl.formatMessage({ id: 'long' }),
+            intl.formatMessage({ id: 'medium' }),
+            intl.formatMessage({ id: 'short' }),
+          ]}
+        />
+      </Collapse>
+    </Container>
+  )
+};
+
+export default SearchQueries;
---
+++ src/tests/components/SearchQueries.test.tsx
@@ -0,0 +1,67 @@
+import React from "react";
+import { render, screen } from "@testing-library/react";
+import { ThemeProvider } from "styled-components";
+import { getTheme } from "../../lib/theme";
+import GlobalStyles from "../../lib/styled/global";
+import { Provider } from "react-redux";
+import store from "../../redux/index";
+import { LangProvider } from "../../lib/localization";
+import { BrowserRouter } from "react-router-dom";
+import { SearchResponse } from "../../models/api/search";
+import { VideoIdResponse } from "../../models/api/video";
+
+import SearchQueries from "../../components/screens/search/components/SearchQueries";
+
+const AllTheProviders = ( { children }: any ) => (
+  <Provider store={store}>
+    <ThemeProvider theme={ getTheme( true ) } >
+      <BrowserRouter>
+        <LangProvider file="root">
+          { children }
+        </LangProvider>
+      </BrowserRouter>
+      <GlobalStyles/>
+    </ThemeProvider>
+  </Provider>
+);
+
+describe( 'SearchQueries Component', () => {
+  it( 'renders correctly', async () => {
+    const result = render(
+      <SearchQueries callback={() => {}}/>,
+      { wrapper: AllTheProviders }
+    );
+
+    await new Promise( resolve => setTimeout( resolve, 100 ) );
+
+    const button = result.container.querySelector( 'button' );
+
+    expect( button ).toBeTruthy();
+  });
+
+  it( 'handles click on search button', async () => {
+    const callback = jest.fn();
+
+    const result = render(
+      <SearchQueries callback={callback}/>,
+      { wrapper: AllTheProviders }
+    );
+
+    await new Promise( resolve => setTimeout( resolve, 100 ) );
+
+    result.container.querySelector( 'button' ).click();
+
+    expect( callback ).toBeCalled();
+  });
+
+  it( 'matches to snapshot', async () => {
+    const result = render(
+      <SearchQueries callback={() => {}} />,
+      { wrapper: AllTheProviders }
+    );
+
+    await new Promise( resolve => setTimeout( resolve, 10 ) );
+
+    expect( result ).toMatchSnapshot();
+  });
+});
