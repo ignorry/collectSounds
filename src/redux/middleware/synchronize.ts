bb5a3b89871070090cf172a940e2cc0f001902d8
s_andrey <weo9345@gmail.com>
2022-08-31 20:51:25 +0300

    Fix synchronize util
    Wrap syncPeers and updateContents with dispatch

--- src/redux/middleware/synchronize.ts
+++ src/redux/middleware/synchronize.ts
@@ -2,7 +2,7 @@ import { AppThunk } from "..";
 import { syncPeers } from "./syncPeers";
 import { updateContents } from "./syncYoutube";
 
-export const synchronize = (): AppThunk => async ( dispatch, getState: Function ) => {
-  syncPeers();
-  updateContents();
+export const synchronize = (): AppThunk => async ( dispatch ) => {
+  dispatch( syncPeers() );
+  dispatch( updateContents() );
 }
