4fae2003a99b8379705cec6f90f7ff44278df0d1
s_andrey <weo9345@gmail.com>
2022-08-31 21:04:47 +0300

    Add call of synchronize util in first render of App

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,9 +1,11 @@
-import React from "react"
+import React, { useEffect } from "react"
 import { connect, ConnectedProps } from "react-redux";
 import { ThemeProvider } from "styled-components";
 import { getTheme } from "../lib/theme";
 import GlobalStyles from "../lib/styled/global";
 import { BrowserRouter } from "react-router-dom";
+import { useDispatch } from "react-redux";
+import { synchronize } from "../redux/middleware/synchronize";
 
 import Root from "./screens/root"
 
@@ -15,13 +17,21 @@ const connector = connect( mapState );
 
 type Props = ConnectedProps<typeof connector>
 
-const App: React.FC<Props> = ( props ) => (
-  <BrowserRouter>
-    <ThemeProvider theme={ getTheme( props.isDark ) } >
-      <Root/>
-      <GlobalStyles/>
-    </ThemeProvider>
-  </BrowserRouter>
-);
+const App: React.FC<Props> = ( props ) => {
+  const dispatch = useDispatch();
+
+  useEffect( () => {
+    dispatch( synchronize() as any );
+  }, [])
+
+  return (
+    <BrowserRouter>
+      <ThemeProvider theme={ getTheme( props.isDark ) } >
+        <Root/>
+        <GlobalStyles/>
+      </ThemeProvider>
+    </BrowserRouter>
+  );
+}
 
 export default connector( App );
